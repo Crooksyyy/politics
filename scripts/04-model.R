38e93fd97d2a4693201d176809388218c27339df
Gavin Crooks <gavin.crooks@mail.utoronto.ca>
2024-03-05 20:40:47 -0500

    paper!

--- scripts/04-model.R
+++ scripts/04-model.R
@@ -9,21 +9,28 @@
 
 
 #### Workspace setup ####
+
 library(tidyverse)
 library(rstanarm)
+library(arrow)
 
 #### Read data ####
-analysis_data <- read_csv("outputs/data/analysis_data.csv")
+analysis_data <- read_parquet("outputs/data/analysis_data.parquet")
+
+set.seed(93)
+
+ces2020_reduced <- 
+  ces2020 |> 
+  slice_sample(n = 1000)
 
 ### Model data ####
 first_model <-
   stan_glm(
-    formula = flying_time ~ length + width,
-    data = analysis_data,
-    family = gaussian(),
+    formula = voted_for ~ gender + education,
+    data = ces2020_reduced,
+    family = binomial(link = "logit"),
     prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
     prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_aux = exponential(rate = 1, autoscale = TRUE),
     seed = 853
   )
 
